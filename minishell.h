231aca5cecca1cd9f41f5c32285c3ba7862fc9ae
Daniel Herszenhaut <dherszen@student.42.rio>
2024-06-21 21:30:59 -0300

    partially adjust the header to the norminette

--- minishell.h
+++ minishell.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dherszen <dherszen@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/21 20:58:53 by dherszen          #+#    #+#             */
+/*   Updated: 2024/06/21 21:05:51 by dherszen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
@@ -10,29 +22,29 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
-typedef struct  s_node
+typedef struct s_node
 {
-    char    **str; //input que será splitado (ainda vamos organizar como iremos separar os comandos)
-    struct  s_node    *next; //ponteiro para a próxima lista para separar os comandos de acordo com '|'(será quando precisaremos usar fork para criar outro processo)
-}       t_node;
+	char	**str; //input que será splitado (ainda vamos organizar como iremos separar os comandos)
+	struct s_node	*next; //ponteiro para a próxima lista para separar os comandos de acordo com '|'(será quando precisaremos usar fork para criar outro processo)
+}	t_node;
 
-enum    e_token
+enum	e_token
 {
-    CMD,
-    ARG,
-    PIPE,
-    HEREDOC,
+	CMD,
+	ARG,
+	PIPE,
+	HEREDOC,
 };
 
-enum    e_builtins
+enum	e_builtins
 {
-    ECHO,
-    CD,
-    PWD,
-    EXPORT,
-    UNSET,
-    ENV,
-    EXIT,
+	ECHO,
+	CD,
+	PWD,
+	EXPORT,
+	UNSET,
+	ENV,
+	EXIT,
 };
 
 #endif
