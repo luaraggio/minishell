af76ae98d8c480a81a1d2c2352a28821ae8cca56
Daniel Herszenhaut <dherszen@student.42.rio>
2024-06-21 21:28:43 -0300

    adjust* main.c to norminette
    the file had several spaces and they were replaced with tabs, but still some part out of the norm

--- main.c
+++ main.c
@@ -1,53 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dherszen <dherszen@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/21 21:06:27 by dherszen          #+#    #+#             */
+/*   Updated: 2024/06/21 21:25:24 by dherszen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
  /*
-    Minishell needs to display a prompt when waiting for a new command.
+	Minishell needs to display a prompt when waiting for a new command.
 
-        We need to:
+		We need to:
 
-        read_command
-        parse_command
-        execute_command
+		read_command
+		parse_command
+		execute_command
 
-        And also:
-        verify_command
-    */
+		And also:
+		verify_command
+	*/
 
 // Variável global para controlar a interrupção ou não do prompt
 unsigned int	prompt_status = 0;
 
-void    sigint_handle(int sig)
+void	sigint_handle(int sig)
 {
-    (void)sig;
-    prompt_status = 1;
-    printf("\n");
-    rl_on_new_line();
-    //replace_line("", 0);
-    rl_redisplay();
+	(void)sig;
+	prompt_status = 1;
+	printf("\n");
+	rl_on_new_line();
+	//replace_line("", 0);
+	rl_redisplay();
 }
 
-int main(void)
+int	main(void)
 {
-    //struct s_node   minishell_data;
-    char    *input;
+	char	*input;
+	//struct s_node   minishell_data;
 
-    signal(SIGINT, sigint_handle);
-    while (42)
-    {
-        if (prompt_status)
-            prompt_status = 0;
-        input = readline("$ ");
-        if (input == NULL)
-        {
-            printf("\n");
-            break;
-        }
-        add_history(input);
-        printf("You entered: %s\n", input);
-        if (ft_strcmp(input, "exit") == 0)
-            break;
-        free(input);
-    }
-    clear_history();
-    free(input);
+	signal(SIGINT, sigint_handle);
+	while (42)
+	{
+		if (prompt_status)
+			prompt_status = 0;
+		input = readline("$ ");
+		if (input == NULL)
+		{
+			printf("\n");
+			break ;
+		}
+		add_history(input);
+		printf("You entered: %s\n", input);
+		if (ft_strcmp(input, "exit") == 0)
+			break ;
+		free(input);
+	}
+	clear_history();
+	free(input);
 }
