f7a1b94ed7287cccb9ffd3396b3fb3bd03a81b2a
Daniel Herszenhaut <dherszen@student.42.rio>
2024-07-10 16:14:03 -0300

    create 'echo' builtin

--- srcs/builtins_1.c
+++ srcs/builtins_1.c
@@ -6,9 +6,20 @@
 /*   By: dherszen <dherszen@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 15:48:45 by dherszen          #+#    #+#             */
-/*   Updated: 2024/07/10 15:48:58 by dherszen         ###   ########.fr       */
+/*   Updated: 2024/07/10 16:07:57 by dherszen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	ft_echo(char *arg)
+{
+	if (arg)
+	{
+		ft_putstr_fd(arg, STDOUT_FILENO);
+		ft_putstr_fd("\n", STDOUT_FILENO);
+	}
+	else
+		ft_putstr_fd("\n", STDOUT_FILENO);
+	return (EXIT_SUCCESS);
+}
