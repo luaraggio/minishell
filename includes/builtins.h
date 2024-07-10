9b9b648cca141a2f3078362531fb1b05ae913e15
Daniel Herszenhaut <dherszen@student.42.rio>
2024-07-10 16:23:33 -0300

    change ft_echo prototype to handle the '-n' argument

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,14 +6,14 @@
 /*   By: dherszen <dherszen@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 16:08:55 by dherszen          #+#    #+#             */
-/*   Updated: 2024/07/10 16:12:40 by dherszen         ###   ########.fr       */
+/*   Updated: 2024/07/10 16:17:12 by dherszen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
 # define BUILTINS_H
 
-int	ft_echo(char *arg);
+int	ft_echo(char **arg);
 
 
 #endif
