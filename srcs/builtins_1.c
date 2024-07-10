afa6aec79aea47bfb9f4163399c0aa4d859a226f
Daniel Herszenhaut <dherszen@student.42.rio>
2024-07-10 16:24:38 -0300

    change ft_echo function to handle the '-n' argument and change to use only builtins.h header (for now)

--- srcs/builtins_1.c
+++ srcs/builtins_1.c
@@ -6,20 +6,20 @@
 /*   By: dherszen <dherszen@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 15:48:45 by dherszen          #+#    #+#             */
-/*   Updated: 2024/07/10 16:07:57 by dherszen         ###   ########.fr       */
+/*   Updated: 2024/07/10 16:21:12 by dherszen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "builtins.h" // trocar depois para "minishell.h"
 
-int	ft_echo(char *arg)
+int	ft_echo(char **args)
 {
-	if (arg)
+	if (args[0])
 	{
-		ft_putstr_fd(arg, STDOUT_FILENO);
+		ft_putstr_fd(args[0], STDOUT_FILENO);
 		ft_putstr_fd("\n", STDOUT_FILENO);
 	}
-	else
+	if (args[1] && !ft_strncmp(args[1], "-n", 2))
 		ft_putstr_fd("\n", STDOUT_FILENO);
 	return (EXIT_SUCCESS);
 }
