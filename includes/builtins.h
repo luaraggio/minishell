0e7b067728d51594c7a3ae76a0ebb7c943773a5b
Daniel Herszenhaut <dherszen@student.42.rio>
2024-07-11 17:22:53 -0300

    update builtins header with ft_env

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,7 +6,7 @@
 /*   By: dherszen <dherszen@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 16:08:55 by dherszen          #+#    #+#             */
-/*   Updated: 2024/07/11 11:34:21 by dherszen         ###   ########.fr       */
+/*   Updated: 2024/07/11 11:54:23 by dherszen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,16 @@
 
 # include "minishell.h" // depois tirar ou colocar # include "minishell.h no lugar de # include "builtins.h em cada arquivo das builtins
 
+typedef struct s_env
+{
+	char			*var;
+	char			*value;
+	struct s_env	*next;
+}	t_env;
+
 int	ft_echo(char **arg);
 int	ft_cd(char **args);
 int	ft_pwd(char **args);
-
+int	ft_env(char **envp);
 
 #endif
